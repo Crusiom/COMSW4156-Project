b3b0f22b1f75ea79921b4f22da6096175a252e36
wanlu <wanlu@umich.edu>
2023-10-19 18:44:27 -0400

    add commnets to the unit test

--- tests/controllers/auth.test.js
+++ tests/controllers/auth.test.js
@@ -2,12 +2,15 @@ const { register, login, logout, getMe } = require('../../controllers/auth.js');
 const User = require('../../models/Users');
 const ErrorResponse = require('../../helpers/errResponse');
 
+// Mocking the User model and ErrorResponse to isolate the tests
 jest.mock('../../models/Users');
 jest.mock('../../helpers/errResponse');
 
 describe('Authentication Controller', () => {
+    // Variables for request, response, and next middleware function
     let req, res, next;
 
+    // Setting up default mocks for req, res, and next before each test
     beforeEach(() => {
         req = { body: {}, user: { id: 'mockUserId' } };
         res = {
@@ -18,8 +21,10 @@ describe('Authentication Controller', () => {
         next = jest.fn();
     });
 
+    // Test suite for the 'register' function
     describe('register', () => {
         it('should register a user successfully', async () => {
+            // Setting up mock request body
             req.body = {
                 name: 'John',
                 email: 'john@example.com',
@@ -28,58 +33,77 @@ describe('Authentication Controller', () => {
                 role: 'user'
             };
 
+            // Mocking User model functions for the test case
             User.findOne.mockResolvedValue(null);
             User.create.mockResolvedValue({
                 getSignedJwtToken: jest.fn().mockReturnValue('mockToken')
             });
 
+            // Invoking the register function
             await register(req, res, next);
+
             // expect(res.status).toHaveBeenCalledWith(200);
             // expect(res.json).toHaveBeenCalled();
         });
 
         it('should not register a user with an existing email', async () => {
+            // Mocking User model's findOne to simulate existing email
             User.findOne.mockResolvedValue({});
             await register(req, res, next);
+
+            // Asserting that ErrorResponse is called due to email conflict
             expect(next).toHaveBeenCalledWith(expect.any(ErrorResponse));
         });
     });
 
+    // Test suite for the 'login' function
     describe('login', () => {
         it('should login a user successfully', async () => {
+            // Setting up mock request body
             req.body = {
                 email: 'john@example.com',
                 password: 'password123'
             };
 
+            // Mocking User model functions for the test case
             User.findOne.mockResolvedValue({
                 matchPassword: jest.fn().mockResolvedValue(true),
                 getSignedJwtToken: jest.fn().mockReturnValue('mockToken')
             });
 
+            // Invoking the login function
             await login(req, res, next);
+
             // expect(res.status).toHaveBeenCalledWith(200);
             // expect(res.json).toHaveBeenCalled();
         });
 
         it('should not login a user with incorrect credentials', async () => {
+            // Setting up mock request body with incorrect password
             req.body = {
                 email: 'john@example.com',
                 password: 'wrongpassword'
             };
 
+            // Mocking User model's findOne function to simulate wrong password scenario
             User.findOne.mockResolvedValue({
                 matchPassword: jest.fn().mockResolvedValue(false)
             });
 
+            // Invoking the login function
             await login(req, res, next);
+
             // expect(next).toHaveBeenCalledWith(expect.any(ErrorResponse));
         });
     });
 
+    // Test suite for the 'logout' function
     describe('logout', () => {
         it('should logout a user', async () => {
+            // Invoking the logout function
             await logout(req, res, next);
+            
+            // Asserting expected response
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
@@ -88,11 +112,16 @@ describe('Authentication Controller', () => {
         });
     });
 
+    // Test suite for the 'getMe' function
     describe('getMe', () => {
         it('should get the authenticated user', async () => {
+            // Mocking User model's findById function
             User.findById.mockResolvedValue({ name: 'John' });
 
+            // Invoking the getMe function
             await getMe(req, res, next);
+
+            // Asserting expected response
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
--- tests/controllers/users.test.js
+++ tests/controllers/users.test.js
@@ -1,10 +1,14 @@
 const { getUsers, getUser, createUser, updateUser, deleteUser } = require('../../controllers/users.js');
+
 const User = require('../../models/Users');
+
+// Helper function to mock the request object
 const mockRequest = (body, params) => ({
     body,
     params,
 });
 
+// Helper function to mock the response object
 const mockResponse = () => {
     const res = {};
     res.status = jest.fn().mockReturnValue(res);
@@ -12,27 +16,37 @@ const mockResponse = () => {
     return res;
 };
 
+// Mocking the next middleware function for error handling
 const mockNext = jest.fn();
 
 describe('User Controller', () => {
+    // Clearing all mocks before each test to ensure a clean slate for each test case
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
+    // Test suite for the 'getUsers' function
     describe('getUsers', () => {
         it('should get all users and return success', async () => {
             const advancedResults = [{ id: 'user1' }, { id: 'user2' }];
             const res = mockResponse();
+            // Simulating middleware behavior where advancedResults is attached to the response object
             res.advancedResults = advancedResults;
+
             await getUsers({}, res, mockNext);
+
+            // Asserting the expected responses
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledWith(advancedResults);
         });
     });
 
+    // Test suite for the 'getUser' function
     describe('getUser', () => {
         it('should get a single user by ID and return success', async () => {
+            // Mock data for a user
             const mockUser = { id: 'user1', profileData: 'some data' };
+            // Mocking the User model's findById function
             User.findById = jest.fn().mockResolvedValue(mockUser);
 
             const req = mockRequest({}, { id: 'user1' });
@@ -40,6 +54,7 @@ describe('User Controller', () => {
 
             await getUser(req, res, mockNext);
 
+            // Asserting the expected responses
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
@@ -48,10 +63,13 @@ describe('User Controller', () => {
         });
     });
 
+    // Test suite for the 'createUser' function
     describe('createUser', () => {
         it('should create a user and return success', async () => {
+            // Mock data for user input and created user
             const mockUserData = { profileData: 'some data' };
             const mockCreatedUser = { id: 'user1', ...mockUserData };
+            // Mocking the User model's create function
             User.create = jest.fn().mockResolvedValue(mockCreatedUser);
 
             const req = mockRequest(mockUserData);
@@ -59,6 +77,7 @@ describe('User Controller', () => {
 
             await createUser(req, res, mockNext);
 
+            // Asserting the expected responses
             expect(res.status).toHaveBeenCalledWith(201);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
@@ -67,10 +86,13 @@ describe('User Controller', () => {
         });
     });
 
+    // Test suite for the 'updateUser' function
     describe('updateUser', () => {
         it('should update a user by ID and return success', async () => {
+            // Mock data for user input and updated user
             const mockUserData = { profileData: 'updated data' };
             const mockUpdatedUser = { id: 'user1', ...mockUserData };
+            // Mocking the User model's findByIdAndUpdate function
             User.findByIdAndUpdate = jest.fn().mockResolvedValue(mockUpdatedUser);
 
             const req = mockRequest(mockUserData, { id: 'user1' });
@@ -78,6 +100,7 @@ describe('User Controller', () => {
 
             await updateUser(req, res, mockNext);
 
+            // Asserting the expected responses
             expect(res.status).toHaveBeenCalledWith(201);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
@@ -86,8 +109,10 @@ describe('User Controller', () => {
         });
     });
 
+    // Test suite for the 'deleteUser' function
     describe('deleteUser', () => {
         it('should delete a user by ID and return success', async () => {
+            // Mocking the User model's findByIdAndDelete function
             User.findByIdAndDelete = jest.fn().mockResolvedValue({});
 
             const req = mockRequest({}, { id: 'user1' });
@@ -95,6 +120,7 @@ describe('User Controller', () => {
 
             await deleteUser(req, res, mockNext);
 
+            // Asserting the expected responses
             expect(res.status).toHaveBeenCalledWith(200);
             expect(res.json).toHaveBeenCalledWith({
                 success: true,
@@ -103,4 +129,3 @@ describe('User Controller', () => {
         });
     });
 });
-
