9db610724247d2a609b847301f07f32070dc0697
Yizhen Zhang <zhangyizhenyizen@gmail.com>
2023-12-16 23:39:17 -0500

    complete user auth iintegration test with real sev

--- tests/integration_tests/userauth.integration.test.js
+++ tests/integration_tests/userauth.integration.test.js
@@ -1,49 +1,52 @@
-const chai = require('chai');
-const nock = require('nock');
-const expect = chai.expect;
+const request = require('supertest');
+const baseUrl = 'http://localhost:3000'; // Replace with your real API base URL
+let token;
+
+beforeAll(async () => {
+    // Log in and get token
+    const loginRes = await request(baseUrl)
+        .post('/api/v1/auth/login')
+        .send({ email: 'dw3033@columbia.edu', password: '123456' });
+    console.log('Login response:', loginRes.body); // Debugging: log the login response
+    token = loginRes.body.token;
+}, 10000); // Increased timeout
 
 describe('User Authentication', () => {
-    const baseApi = 'http://localhost:3000';
+    it('should return 400 for invalid registration data', async () => {
+        const invalidUserData = {
+            name: '',
+            email: 'invalidemail',
+            password: '123',
+            app: '123123',
+            role: 'user',
+        };
 
-    beforeEach(() => {
-        nock.cleanAll();
+        const res = await request(baseUrl).post('/api/v1/auth/register').send(invalidUserData);
+
+        expect(res.statusCode).toEqual(400); // 400 for invalid data
+        // Add more assertions as needed based on your response structure
     });
 
-    // User Registration Test
-    describe('/POST Register', () => {
-        it('it should mock successful registration', (done) => {
-            nock(baseApi).post('/api/v1/auth/register').reply(200, {
-                success: true,
-                token: 'mockToken',
-            });
-            done();
-        });
+    it('should be successful login', async () => {
+        const loginData = {
+            email: 'dw3033@columbia.edu',
+            password: '123456',
+        };
 
-        it('it should mock registration failure due to existing email', (done) => {
-            nock(baseApi).post('/api/v1/auth/register').reply(400, {
-                success: false,
-                error: 'User with this email already exists',
-            });
-            done();
-        });
+        const res = await request(baseUrl).post('/api/v1/auth/login').send(loginData);
+
+        expect(res.statusCode).toEqual(200); // Assuming 200 is the success status code for login
+        // Add more assertions as needed based on your response structure
     });
 
-    // User Login Test
-    describe('/POST Login', () => {
-        it('it should mock successful login', (done) => {
-            nock(baseApi).post('/api/v1/auth/login').reply(200, {
-                success: true,
-                token: 'mockToken',
-            });
-            done();
-        });
+    it('should be login failure with incorrect credentials', async () => {
+        const wrongLoginData = {
+            email: 'wrong@example.com',
+            password: 'wrongpassword',
+        };
 
-        it('it should mock login failure with incorrect credentials', (done) => {
-            nock(baseApi).post('/api/v1/auth/login').reply(401, {
-                success: false,
-                error: 'Invalid Credentials',
-            });
-            done();
-        });
+        const res = await request(baseUrl).post('/api/v1/auth/login').send(wrongLoginData);
+
+        expect(res.statusCode).toEqual(401); // 401 for invalid credentials
     });
 });
