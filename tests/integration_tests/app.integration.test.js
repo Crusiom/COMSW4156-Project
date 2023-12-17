c049083e19aabcb5f9c4995370af45f0c2b54c87
Yizhen Zhang <zhangyizhenyizen@gmail.com>
2023-12-16 23:11:22 -0500

    finished app.integration.test.js using running sev

--- tests/integration_tests/app.integration.test.js
+++ tests/integration_tests/app.integration.test.js
@@ -1,35 +1,47 @@
-const app = require('../../models/Apps');
 const request = require('supertest');
+const baseUrl = 'http://localhost:3000'; // Replace with your real API base URL
+let token;
+let createdAppId;
 
-describe('Auth Endpoints', () => {
+beforeAll(async () => {
+    // Log in and get token
+    const loginRes = await request(baseUrl)
+        .post('/api/v1/auth/login')
+        .send({ email: 'dw3033@columbia.edu', password: '123456' });
+    console.log('Login response:', loginRes.body); // Debugging: log the login response
+    token = loginRes.body.token;
+}, 10000); // Increased timeout
+
+describe('App Management', () => {
     it('should create a new app', async () => {
-        const res = request(app).post('/api/v1/apps').send({
-            /* app data */
-        });
+        const appData = {
+            title: 'New App Title',
+            commentEnabled: true,
+            eventEnabled: true,
+            visibleEnabled: true,
+        };
 
-        expect(res.statusCode).toEqual(); // undefined
-        // Additional assertions as needed
+        const res = await request(baseUrl).post('/api/v1/apps').set('Authorization', `Bearer ${token}`).send(appData);
+
+        expect(res.statusCode).toEqual(200); // Assuming 200 is the success status code for app creation
+        expect(res.body.data.title).toEqual(appData.title); // Verifying the created app data
+        createdAppId = res.body.data._id; // Store the ID of the created app for later tests
     });
-    it('should register a user successfully', async () => {
-        const res = request(app).post('/api/v1/auth/register').send({
-            name: 'Test User',
-            email: 'test@example.com',
-            password: 'password123',
-            app: 'Test App',
-            role: 'User',
-        });
 
-        expect(res.statusCode).toEqual();
-    });
-    it('should return 400 for invalid registration data', async () => {
-        const res = request(app).post('/api/v1/auth/register').send({
-            name: '',
-            email: 123,
-            password: '41564156',
-            app: '123123',
-            role: 'user',
-        });
+    // Additional test for updating an app
+    it('should update an existing app', async () => {
+        const updatedData = {
+            title: 'Updated App Title',
+            commentEnabled: false,
+            // Update other fields as required
+        };
 
-        expect(res.statusCode).toEqual(); // undefiend
+        const res = await request(baseUrl)
+            .put(`/api/v1/apps/${createdAppId}`) // Using the ID of the created app
+            .set('Authorization', `Bearer ${token}`)
+            .send(updatedData);
+
+        expect(res.statusCode).toEqual(201); // Assuming 201 is the success status code for updates
+        expect(res.body.data.title).toEqual(updatedData.title); // Verifying the updated app data
     });
 });
